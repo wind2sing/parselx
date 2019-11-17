68aa978e4f2ef72ef8ede2ffbe37150192d4ba1c
zireael <zireael.me@gmail.com>
2019-11-17 15:36:00 +0800

    feat: support divider in list

--- parselx/process.py
+++ parselx/process.py
@@ -1,4 +1,4 @@
-from .utils import parse_query, partial
+from .utils import parse_query, enhanced_css
 from .x_instance import x
 
 
@@ -8,36 +8,49 @@ def _parsel_one(sel, query: str, **kwargs):
         raise TypeError(f"Query should be string: {query}")
     q = parse_query(query)
     lang = q["lang"]
+
+    # extract values
     if lang == "xpath":
         val = getattr(getattr(sel, lang)(q["query"], **kwargs), q["meth"])()
     else:
-        val = getattr(getattr(sel, lang)(q["query"]), q["meth"])()
-    return val
+        query = enhanced_css(q["query"])
+        val = getattr(getattr(sel, lang)(query), q["meth"])()
 
-
-def _process_by_funcs(val, funcs=None):
-    """Process field with functions"""
-
-    for func in funcs or []:
-        if isinstance(func, str):
-            li = func.split(":", 1)
-            func_name = li[0]
-            args = li[1].split(",") if len(li) == 2 else []
-            func = partial(x.filters[func_name], *args, new_args_before=True)
-        val = func(val)
+    # apply filters function
+    filters = q["filters"]
+    for filt in filters:
+        func = x.filters[filt["name"]]
+        val = func(val, *filt["args"])
     return val
 
 
 def process(sel, queries, **kwargs):
-    if isinstance(queries, dict):
-        return {k: process(sel, v, **kwargs) for k, v in queries.items()}
 
     if isinstance(queries, list):
+        if should_divide(queries):
+            divider = queries.pop(0)
+            d_query = parse_query(divider)
+            return [
+                process(sel, queries)
+                for sel in getattr(sel, d_query["lang"])(d_query["query"])
+            ]
+
         query = queries[0]
         funcs = queries[1:]
         val = process(sel, query, **kwargs)
         return _process_by_funcs(val, funcs)
+    elif isinstance(queries, dict):
+        return {k: process(sel, v, **kwargs) for k, v in queries.items()}
     else:
         query = queries
         return _parsel_one(sel, query, **kwargs)
 
+
+def _process_by_funcs(val, funcs=None):
+    for func in funcs or []:
+        val = func(val)
+    return val
+
+
+def should_divide(queries: list):
+    return len(queries) > 1 and not callable(queries[1])
